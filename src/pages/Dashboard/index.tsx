45e8b14760295f4fea3c982abad57ce4908b4a10
Marcos Souza <bytecore@gmail.com>
2021-02-07 00:23:42 -0300

    Salvando no localstorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
@@ -18,7 +18,19 @@ const Dashboard: React.FC = () => {
 
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@githubExplorer:repositores');
+
+        if(storagedRepositories){
+            return JSON.parse(storagedRepositories);
+        }else {
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem('@githubExplorer:repositores', JSON.stringify(repositories))
+    }, [repositories]);
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
